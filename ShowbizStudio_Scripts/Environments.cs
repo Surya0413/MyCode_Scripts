5e53e908f3735ab07df5a83de646ff269c37f614
Surya Sasidhar <suryaraosdet@gmail.com>
2022-06-28 11:39:48 +0530

    Change in Environment file

--- ShowbizStudio_Scripts/Environments.cs
+++ ShowbizStudio_Scripts/Environments.cs
@@ -13,35 +13,49 @@ namespace ShowbizStudio_Scripts
         [Test]
         public void Showbizstudio_QA_Parameters()
         {
-           DataHandler.Instance.InitializeParameter("URL", @"https://dev.show.com/");                       
+            DataHandler.Instance.InitializeParameter("URL", @"https://qa.showbizstudio.com/");
             DataHandler.Instance.InitializeParameter("Environment", "QA");
-         //   DataHandler.Instance.InitializeParameter("Browser", "Edge");
-            DataHandler.Instance.InitializeParameter("Browser", "Chrome");
-            DataHandler.Instance.InitializeParameter("Username", "surya.sasidhar@gmail.com");
-            DataHandler.Instance.InitializeParameter("Password", "Monday");           
+              DataHandler.Instance.InitializeParameter("Browser", "Edge");
+           // DataHandler.Instance.InitializeParameter("Browser", "Chrome");
+            DataHandler.Instance.InitializeParameter("Username", "surya.sasidhar@castandcrew.com");
+            DataHandler.Instance.InitializeParameter("Password", "Tuesday@17122012sk");
+            DataHandler.Instance.InitializeParameter("RUsername", @"corp.media-services.com\reports");
+            DataHandler.Instance.InitializeParameter("RPassword", "@ShowbizStudioOnly");
 
         }
 
         [Test]
         public void Showbizstudio_Dev_Parameters()
         {
-             DataHandler.Instance.InitializeParameter("URL", @"https://dev.show.com/");                       
-            DataHandler.Instance.InitializeParameter("Environment", "QA");
-         //   DataHandler.Instance.InitializeParameter("Browser", "Edge");
-            DataHandler.Instance.InitializeParameter("Browser", "Chrome");
-            DataHandler.Instance.InitializeParameter("Username", "surya.sasidhar@gmail.com");
-            DataHandler.Instance.InitializeParameter("Password", "Monday");     
+            DataHandler.Instance.InitializeParameter("URL", @"https://dev.showbizstudio.com/");
+            DataHandler.Instance.InitializeParameter("Environment", "DEV");
+            DataHandler.Instance.InitializeParameter("Browser", "Edge");
+            // DataHandler.Instance.InitializeParameter("Browser", "Chrome");
+            DataHandler.Instance.InitializeParameter("Username", "surya.sasidhar@castandcrew.com");
+            DataHandler.Instance.InitializeParameter("Password", "Tuesday@17122012sk");
         }
 
         [Test]
         public void Showbizstudio_Stage_Parameters()
         {
-           DataHandler.Instance.InitializeParameter("URL", @"https://dev.show.com/");                       
-            DataHandler.Instance.InitializeParameter("Environment", "QA");
-         //   DataHandler.Instance.InitializeParameter("Browser", "Edge");
+            DataHandler.Instance.InitializeParameter("URL", @"https://stg.showbizstudio.com/");
+            DataHandler.Instance.InitializeParameter("Environment", "STG");
             DataHandler.Instance.InitializeParameter("Browser", "Chrome");
-            DataHandler.Instance.InitializeParameter("Username", "surya.sasidhar@gmail.com");
-            DataHandler.Instance.InitializeParameter("Password", "Monday");     
+            // DataHandler.Instance.InitializeParameter("Browser", "Edge");
+            DataHandler.Instance.InitializeParameter("Username", "surya.sasidhar@castandcrew.com");
+            DataHandler.Instance.InitializeParameter("Password", "Tuesday@17122012sk");
+        }
+
+
+        [Test]
+        public void ClientShowBiz_Stage()
+        {
+            DataHandler.Instance.InitializeParameter("URL", @"https://clientportalstg.showbizstudio.com");
+            DataHandler.Instance.InitializeParameter("Environment", "STG");
+            DataHandler.Instance.InitializeParameter("Browser", "Chrome");
+            // DataHandler.Instance.InitializeParameter("Browser", "Edge");
+            DataHandler.Instance.InitializeParameter("Username", "umut@mediaservices.com");
+            DataHandler.Instance.InitializeParameter("Password", "Manuyghur802103333@");
         }
 
         [TearDown]
